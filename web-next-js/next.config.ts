40c0ec8a8cee1816a27d6d9b14190e989497bfda
everythingfades <raymondguhonglei@163.com>
2024-12-29 19:47:12 +0000

    fix (attempt 5) CORS issues

--- web-next-js/next.config.ts
+++ web-next-js/next.config.ts
@@ -1,10 +1,41 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+const CORS_HEADERS = [
+  {
+    key: "Access-Control-Allow-Credentials",
+    value: "true",
+  },
+  {
+    key: "Access-Control-Allow-Origin",
+    value: "*", // Change this to your specific origin in production
+  },
+  {
+    key: "Access-Control-Allow-Methods",
+    value: "GET, DELETE, PATCH, POST, PUT",
+  },
+  {
+    key: "Access-Control-Allow-Headers",
+    value: "*",
+  },
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     unoptimized: true
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Add headers for all API routes
+        headers: CORS_HEADERS,
+      },
+      {
+        source: "/specific", // Add headers for a specific route
+        headers: CORS_HEADERS,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
