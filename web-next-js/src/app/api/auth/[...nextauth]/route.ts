0ea0a4f1ecd9b318bc415431737886f0a9dcb805
everythingfades <raymondguhonglei@163.com>
2024-12-30 03:43:20 +0000

    feat: add logout; fix: fixed invalid export

--- web-next-js/src/app/api/auth/[...nextauth]/route.ts
+++ web-next-js/src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,7 @@
-import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { compare } from "bcryptjs";
-// import { findUsername } from "@/utils/api"; // Your custom function to find users
-// import type { User as NextAuthUser } from "next-auth";
-import { NextApiRequest, NextApiResponse } from "next";
-import { authOptions } from "../options";
+import NextAuth from 'next-auth';
+import { authOptions } from '../options';
 
-// Handling the POST request
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  // console.log("GET request received");
-  return NextAuth(req, res, authOptions); // Pass both req and res
-}
+const handler = NextAuth(authOptions);
 
-// Export the GET method
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  // console.log("GET request received");
-  return NextAuth(req, res, authOptions); // Call NextAuth to handle the GET request
-}
+// Exporting the handler for both GET and POST methods
+export { handler as GET, handler as POST };
--- web-next-js/src/app/api/auth/error/page.tsx
+++ web-next-js/src/app/api/auth/error/page.tsx
@@ -1,24 +1,24 @@
-// 'use client'; // Indicate that this is a Client Component
+'use client'; // Indicate that this is a Client Component
 
 // import { useSearchParams } from 'next/navigation';
 
-// const ErrorPage = () => {
-//   const searchParams = useSearchParams(); // Access search parameters
+const ErrorPage = () => {
+  // const searchParams = useSearchParams(); // Access search parameters
 
-//   // Parse the error from the URL
-//   const error = searchParams.get('error');
+  // Parse the error from the URL
+  // const error = searchParams.get('error');
 
-//   return (
-//     <div>
-//       <h1>Error</h1>
-//       {error ? (
-//         <p>{decodeURIComponent(error)}</p> // Display the error message
-//       ) : (
-//         <p>An unknown error occurred.</p>
-//       )}
-//       <a href="/auth/signin">Go back to Sign In</a>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>Error</h1>
+      {/* {error ? (
+        <p>{decodeURIComponent(error)}</p> // Display the error message
+      ) : (
+        <p>An unknown error occurred.</p>
+      )} */}
+      <a href="/login">Go back to Log In</a>
+    </div>
+  );
+};
 
-// export default ErrorPage;
+export default ErrorPage;
