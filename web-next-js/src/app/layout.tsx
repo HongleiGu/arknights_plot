301a518f39b553bba7bafbe1d4d14d5e2a05edea
everythingfades <raymondguhonglei@163.com>
2025-01-19 14:05:27 +0000

    fix: make the header able to repsond to different size, but still need to make a side bar on mobile rendering

--- web-next-js/src/app/layout.tsx
+++ web-next-js/src/app/layout.tsx
@@ -2,9 +2,9 @@
 
 import Image from "next/image";
 import Link from "next/link";
-// import babel_logo from "../../public/assets/pics/babel_logo.png"
-import './globals.css'; // Your global styles
-import "./page.scss"
+import './base.scss'; // Your global styles
+// import "./page.scss"
+import babel_logo from "@assets/pics/babel_logo.png"
 import useLoginStore from "./store/login";
 import { signOut } from "next-auth/react";
 
@@ -19,70 +19,69 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const store = useLoginStore()
   return (
     <html>
-      <body>
-      <header className="base">
-        <div className="base img-wrapper">
-          <Image src={`/assets/pics/babel_logo.png`} width={100} height={100} alt="babel logo"/>
-        </div>
-        <div className="base router-link-wrapper">
-          {
-            links.map(it => 
-              <Link href={it.path} className="base routerlink" key={it.ENname}>
-                <div className="base routerlink-inner-wrapper">
-                  <p className="base routerlink-en">{it.ENname}</p>
-                  <p className="base routerlink-ch">{it.CNname}</p>
+        <body className="flex flex-col w-full">
+            <header className="flex flex-row relative h-[80px] w-full z-10 bg-black">
+                {/* <Image
+                    className="" // Set width to 10%
+                    src={babel_logo} 
+                    width={50}
+                    height={50}
+                    alt="babel logo"
+                    priority={true}
+                /> */}
+                <div className="relative p-10 z-[1000] h-full w-[10%] opacity-100">
+                  <Image 
+                    src={`/assets/pics/babel_logo.png`} 
+                    width={100} 
+                    height={100} 
+                    alt="babel logo" 
+                    className="absolute top-0 left-0 max-w-full max-h-full object-contain" // Use w-full for responsive width and h-auto to maintain aspect ratio
+                  />
                 </div>
-              </Link>
-            )
-          }
-        </div>
-        {/* <div className="base router-link-wrapper">
-          <Link href="/home" className="base routerlink">
-            <div className="base routerlink-inner-wrapper">
-              <p className="base routerlink-en">HOME</p>
-              <p className="base routerlink-ch">首页</p>
+                <div className="relative flex flex-row w-[80%] justify-around border-collapse">
+                    {links.map(it => (
+                        <Link 
+                          href={it.path} 
+                          className="relative flex-1 flex flex-col items-start h-full justify-between p-[10px] border-solid border-white hidden sm:flex" 
+                          key={it.ENname}
+                        >
+                          <div className="relative flex flex-col justify-around h-full w-full">
+                            <p className="routerlink-en text-center">{it.ENname}</p>
+                            <p className="routerlink-ch text-center">{it.CNname}</p>
+                          </div>
+                        </Link>
+                    ))}
+                </div>
+                <div className="relative flex flex-row w-[30%]"> {/* Remaining 30% width */}
+                    <Link 
+                        href="/login" 
+                        className="relative flex-1 flex flex-col items-start h-full justify-between p-[10px]" 
+                        id="login"
+                    >
+                        <div className="relative flex flex-col justify-around h-full w-full">
+                            {store.user ? 
+                                <p className="routerlink-ch text-center">当前用户: {store.user?.name}</p> 
+                                :
+                                <p className="routerlink-ch text-center">登录/注册</p>
+                            }
+                        </div>
+                    </Link>
+                    <a 
+                        className="relative flex-1 flex flex-col h-full justify-around p-[10px]"
+                        onClick={async () => { 
+                            store.setUser(null); 
+                            await signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_URL}/home` }); 
+                            store.verifyJwt();
+                        }}
+                    >
+                        <span className="relative routerlink-ch">登出</span>
+                    </a>
+                </div>
+            </header>
+            <div className="base routerview">
+                {children}
             </div>
-          </Link>
-          <Link href="/navigate" className="base routerlink">
-            <div className="base routerlink-inner-wrapper">
-              <p className="base routerlink-en">NAVIGATE</p>
-              <p className="base routerlink-ch">导航页</p>
-            </div>
-          </Link>
-          <Link href="/home" className="base routerlink">
-            <div className="base routerlink-inner-wrapper">
-              <p className="base routerlink-en">DOCUMENT</p>
-              <p className="base routerlink-ch">文档页</p>
-            </div>
-          </Link>
-          <Link href="/home" className="base routerlink">
-            <div className="base routerlink-inner-wrapper">
-              <p className="base routerlink-en">EXTRA</p>
-              <p className="base routerlink-ch">扩展</p>
-            </div>
-          </Link> 
-        </div> */}
-        <Link href="/login" className="base routerlink" id="login">
-          <div className="base routerlink-inner-wrapper">
-            {/* <Image src={null} alt="" id="user-pic"/> */}
-            { store.user ? 
-              <p className="base routerlink-ch">当前用户:{store.user?.name}</p> 
-              :
-              <p className="base routerlink-ch">登录/注册</p>
-            }
-          </div>
-        </Link>
-        <div className="base logout">
-          <a className="base routerlink-ch"
-            onClick={async () => {store.setUser(null); await signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_URL}/home` }); store.verifyJwt()}}
-          >登出</a>
-        </div>
-      </header>
-      {/* <RouterView className="base routerview"/> */}
-      <div className="base routerview">
-        {children}
-      </div>
-    </body>
+        </body>
     </html>
   );
 }
